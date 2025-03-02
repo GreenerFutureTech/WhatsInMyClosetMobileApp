37f9dcca447c4bbfe5533e12a91d1941e915b5cc
Maísa Wolff Resplande <maisawres@gmail.com>
2025-03-02 15:39:01 -0500

    Update modifiers for OutfitComposable.

--- WhatsInMyCloset/composeApp/src/commonMain/kotlin/org/greenthread/whatsinmycloset/core/ui/components/outfits/OutfitComposable.kt
+++ WhatsInMyCloset/composeApp/src/commonMain/kotlin/org/greenthread/whatsinmycloset/core/ui/components/outfits/OutfitComposable.kt
@@ -1,12 +1,19 @@
 package org.greenthread.whatsinmycloset.core.ui.components.outfits
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil3.compose.rememberAsyncImagePainter
@@ -54,7 +61,16 @@ val itemRepository: Map<String, ClothingItem> = mapOf(
 
 @Composable
 fun OutfitComposable(outfit: Outfit) {
-    Box(modifier = Modifier.fillMaxSize()) {
+    Box(modifier = Modifier
+        .height(150.dp)
+        .fillMaxWidth()
+        .background(
+            color = Color.White,
+            shape =  RoundedCornerShape(8.dp)
+        )
+        .clip(RoundedCornerShape(8.dp))
+//        .padding(8.dp)
+    ) {
         // Resolve itemIds to OutfitItem objects
         var x = 5f
         var y = 5f
