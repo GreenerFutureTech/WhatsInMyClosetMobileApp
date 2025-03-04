3d3ec77059daf6ce332d8cf22e6d02b60f868bfb
Maísa Wolff Resplande <maisawres@gmail.com>
2025-03-04 01:12:38 -0500

    Update preview.

--- WhatsInMyCloset/composeApp/src/androidMain/kotlin/org/greenthread/whatsinmycloset/social/SocialTabPreview.kt
+++ WhatsInMyCloset/composeApp/src/androidMain/kotlin/org/greenthread/whatsinmycloset/social/SocialTabPreview.kt
@@ -1,46 +1,18 @@
 package org.greenthread.whatsinmycloset.social
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import org.greenthread.whatsinmycloset.core.domain.models.Account
-import org.greenthread.whatsinmycloset.core.domain.models.Outfit
-import org.greenthread.whatsinmycloset.core.ui.components.posts.Post
-import org.greenthread.whatsinmycloset.core.ui.components.posts.SocialFeedScreen
-import org.greenthread.whatsinmycloset.core.ui.components.posts.getCurrentDate
-
+import org.greenthread.whatsinmycloset.features.tabs.social.SocialTabScreen
 
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
 fun SocialTab() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+    var showContent by remember { mutableStateOf(false) }
 
-        // Create a user profile
-        val user = Account("user123", "rachelg")
-
-        // Generate outfits
-        for (i in 0 until 10) {
-            val newLook =  Outfit("$i", "Look${i}", setOf("1", "2"))
-            user.addOutfit(newLook)
-        }
-
-        // Generate posts
-        val postsList = mutableListOf<Post>()
-
-        for (i in 0 until 10) {
-            val post = user.getOutfit("${i}")?.let { Post(190.dp, user, it, getCurrentDate()) }
-            if (post != null) {
-                postsList.add(post)
-            }
-        }
-
-        SocialFeedScreen(postsList)
-    }
+    SocialTabScreen({})
 }
 
