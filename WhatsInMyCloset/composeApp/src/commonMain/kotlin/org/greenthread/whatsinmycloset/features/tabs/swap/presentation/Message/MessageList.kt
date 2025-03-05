3d46a1c2329e25a03c0b78a70fcccd15664c6c1d
Yujin <wjd2000db@gmail.com>
2025-03-05 15:53:24 -0500

    update ui

--- WhatsInMyCloset/composeApp/src/commonMain/kotlin/org/greenthread/whatsinmycloset/features/tabs/swap/presentation/Message/MessageList.kt
+++ WhatsInMyCloset/composeApp/src/commonMain/kotlin/org/greenthread/whatsinmycloset/features/tabs/swap/presentation/Message/MessageList.kt
@@ -1,7 +1,10 @@
 package org.greenthread.whatsinmycloset.features.tabs.swap.presentation.Message
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,11 +12,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -28,26 +35,43 @@ fun MessageList(
     user: UserDto,
     lastMessage: String,
     onClick: () -> Unit
-){
-    Row(
+) {
+    Button(
+        onClick = { onClick() },
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { onClick() }
-            .padding(8.dp),
-        verticalAlignment = Alignment.CenterVertically
+            .padding(vertical = 4.dp)
+            .background(Color.White),
+        contentPadding = PaddingValues(8.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent)
     ) {
-        @OptIn(ExperimentalResourceApi::class) // TEMP for /drawble image
-        AsyncImage(
-            model = user.profilePicture ?: Res.getUri("drawable/defaultUser.png"),
-            contentDescription = "Profile Image",
-            modifier = Modifier
-                .size(50.dp)
-                .clip(CircleShape)
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-        Column {
-            Text(text = user.name, fontWeight = FontWeight.Bold, fontSize = 16.sp)
-            Text(text = lastMessage, fontSize = 14.sp, color = Color.Gray)
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            @OptIn(ExperimentalResourceApi::class) // TEMP for /drawable image
+            AsyncImage(
+                model = user.profilePicture ?: Res.getUri("drawable/defaultUser.png"), // TODO: REMOVE drawable
+                contentDescription = "Profile Image",
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .border(1.dp, Color.LightGray, CircleShape)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column {
+                Text(
+                    text = user.username,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = Color.Black
+                )
+                Text(
+                    text = lastMessage,
+                    fontSize = 14.sp,
+                    color = Color.Gray
+                )
+            }
         }
     }
 }
