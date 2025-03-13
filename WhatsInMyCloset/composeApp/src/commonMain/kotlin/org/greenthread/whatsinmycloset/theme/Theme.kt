538d08286ca3565293ab55cd42197fb65fdb2cd8
Maísa Wolff Resplande <maisawres@gmail.com>
2025-03-13 10:53:05 -0400

    Fix Theme's name.

--- WhatsInMyCloset/composeApp/src/commonMain/kotlin/org/greenthread/whatsinmycloset/theme/Theme.kt
+++ WhatsInMyCloset/composeApp/src/commonMain/kotlin/org/greenthread/whatsinmycloset/theme/Theme.kt
@@ -249,7 +249,7 @@ val unspecified_scheme = ColorFamily(
 )
 
 @Composable
-fun AppTheme(
+fun WhatsInMyClosetTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
