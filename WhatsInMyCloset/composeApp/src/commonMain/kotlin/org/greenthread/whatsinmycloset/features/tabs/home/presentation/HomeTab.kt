e72693938bad183a57749b6ac6d4be8dbe7a3444
Maísa Wolff Resplande <maisawres@gmail.com>
2025-03-14 17:11:08 -0400

    Refactor category item with new layout.

--- WhatsInMyCloset/composeApp/src/commonMain/kotlin/org/greenthread/whatsinmycloset/features/tabs/home/presentation/HomeTab.kt
+++ WhatsInMyCloset/composeApp/src/commonMain/kotlin/org/greenthread/whatsinmycloset/features/tabs/home/presentation/HomeTab.kt
@@ -1,5 +1,6 @@
 package org.greenthread.whatsinmycloset.features.tabs.home.presentation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,22 +10,29 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.material.icons.filled.ShoppingCart
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material.icons.rounded.AddCircle
 import androidx.compose.material.icons.rounded.Build
 import androidx.compose.material.icons.rounded.DateRange
 import androidx.compose.material.icons.rounded.Home
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -35,7 +43,15 @@ import org.greenthread.whatsinmycloset.core.domain.models.User
 import org.greenthread.whatsinmycloset.core.ui.components.listItems.LazyGridColourBox
 import org.greenthread.whatsinmycloset.core.ui.components.listItems.generateRandomItems
 import org.greenthread.whatsinmycloset.theme.WhatsInMyClosetTheme
+import org.jetbrains.compose.resources.StringResource
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import whatsinmycloset.composeapp.generated.resources.Res
+import whatsinmycloset.composeapp.generated.resources.category_label_accessories
+import whatsinmycloset.composeapp.generated.resources.category_label_bottoms
+import whatsinmycloset.composeapp.generated.resources.category_label_footwear
+import whatsinmycloset.composeapp.generated.resources.category_label_tops
+import whatsinmycloset.composeapp.generated.resources.see_all_button
 
 @Composable
 @Preview
@@ -114,7 +130,7 @@ fun HomeTabScreen(
         CategoriesSection({})
 
         Text(
-            wardrobe?.wardrobeName ?: "No wardrobe found!",
+            wardrobe?.wardrobeName ?: stringResource(Res.string.no_wardrobe_found),
             modifier = Modifier.align(Alignment.CenterHorizontally))
 
         Spacer(modifier = Modifier.height(25.dp))
@@ -136,7 +152,7 @@ fun WardrobeHeader(itemCount: Int) {
     Column(modifier = Modifier.fillMaxWidth()) {
         Text(
             text = "$itemCount items in your Wardrobe",
-            style = MaterialTheme.typography.h6,
+            style = MaterialTheme.typography.titleSmall,
             modifier = Modifier
                 .padding(16.dp)
                 .align(Alignment.CenterHorizontally)
@@ -144,32 +160,33 @@ fun WardrobeHeader(itemCount: Int) {
     }
 }
 
+private data class ImageVectorStringPair(
+    val icon: ImageVector,
+    val text: StringResource
+)
+
 @Composable
 fun CategoriesSection(onCategoryClick: (String) -> Unit) {
-    Row(
+    val itemCategories = listOf(
+        Icons.Rounded.Home to Res.string.category_label_tops,
+        Icons.Default.Add to Res.string.category_label_bottoms,
+        Icons.Default.PlayArrow to Res.string.category_label_accessories,
+        Icons.Default.Call to Res.string.category_label_footwear
+    ).map { ImageVectorStringPair(it.first, it.second) }
+
+    LazyRow(
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        // Tops Category
-        CategoryItem(
-            icon = Icons.Rounded.Home,
-            text = "Tops",
-            onClick = { onCategoryClick("Tops") }
-        )
-        // Bottoms Category
-        CategoryItem(
-            icon = Icons.Default.Add,
-            text = "Bottoms",
-            onClick = { onCategoryClick("Bottoms") }
-        )
-        // Accessories Category
-        CategoryItem(
-            icon = Icons.Default.PlayArrow,
-            text = "Accessories",
-            onClick = { onCategoryClick("Accessories") }
-        )
+        items(itemCategories) { item ->
+            CategoryItem(
+                icon = item.icon,
+                text = stringResource(item.text),
+                onClick = { onCategoryClick(item.text.toString()) },
+            )
+        }
     }
 }
 
@@ -197,11 +214,25 @@ fun BottomButtonsRow(
             text = "Outfit of the Day",
             onClick = {  }
         )
-        CategoryItem(
-            icon = Icons.Rounded.AddCircle,
-            text = "New Item",
+
+        AddNewItem(
             onClick = { launchAddItemScreen() }
         )
+//        CategoryItem(
+//            icon = Icons.Rounded.AddCircle,
+//            text = "New Item",
+//            onClick = { launchAddItemScreen() }
+//        )
+    }
+}
+
+@Composable
+fun AddNewItem(onClick: () -> Unit) {
+    ExtendedFloatingActionButton(
+        onClick = { onClick() }
+    ){
+        Icon(Icons.Filled.Add, "Add new item")
+        Text(text = "New Item")
     }
 }
 
@@ -213,21 +244,31 @@ fun CategoryItem(icon: ImageVector?, text: String?, onClick: (() -> Unit)? = nul
             .clickable(enabled = onClick != null) { onClick?.invoke() },
         horizontalAlignment = Alignment.CenterHorizontally // Center the icon and text
     ) {
-        // Icon
-        icon?.let {
-            Icon(
-                imageVector = icon,
-                contentDescription = text, // Accessibility description
-                modifier = Modifier.size(48.dp), // Set icon size
-                tint = MaterialTheme.colors.onSurface // Use theme color for the icon
-            )
-            Spacer(modifier = Modifier.height(8.dp)) // Space between icon and text
-            // Text
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(88.dp)
+                .background(color = MaterialTheme.colorScheme.surfaceVariant, shape = CircleShape)
+        ) {
+            // Icon
+            icon?.let {
+                Icon(
+                    imageVector = icon,
+                    contentDescription = text, // Accessibility description
+                    modifier = Modifier.size(48.dp)
+                )
+            }
         }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Label
         text?.let {
             Text(
                 text = text,
-                style = MaterialTheme.typography.body2
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier
+                    .paddingFromBaseline(top = 24.dp, bottom = 8.dp)
             )
         }
     }
@@ -235,12 +276,12 @@ fun CategoryItem(icon: ImageVector?, text: String?, onClick: (() -> Unit)? = nul
 
 @Composable
 fun SeeAllButton(onClick: () -> Unit) {
-    Button(
+    TextButton(
         onClick = onClick,
         modifier = Modifier.padding(4.dp),
-        colors = ButtonDefaults.buttonColors(MaterialTheme.colors.secondary)
+        //colors = ButtonDefaults.buttonColors(MaterialTheme.colors.secondary)
     ) {
-        Text(text = "See All")
+        Text(text = stringResource(Res.string.see_all_button))
     }
 }
 
