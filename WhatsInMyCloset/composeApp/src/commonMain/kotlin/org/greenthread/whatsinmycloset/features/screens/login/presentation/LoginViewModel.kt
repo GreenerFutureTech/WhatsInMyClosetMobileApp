541ce25ceb2d2a0d488216fa9e0dd13e9ecc6eed
Jacob <jross9111@conestogac.on.ca>
2025-03-21 14:10:16 -0400

    comments

--- WhatsInMyCloset/composeApp/src/commonMain/kotlin/org/greenthread/whatsinmycloset/features/screens/login/presentation/LoginViewModel.kt
+++ WhatsInMyCloset/composeApp/src/commonMain/kotlin/org/greenthread/whatsinmycloset/features/screens/login/presentation/LoginViewModel.kt
@@ -27,8 +27,12 @@ class LoginViewModel(
     private val notificationsViewModel: NotificationsViewModel
 ): ViewModel() {
     private val auth = Firebase.auth
+
+    // View Model State variables to track login details
     private val _state = mutableStateOf(LoginState(isLoading = true))
     val state by _state
+
+    // Lambda Functions that can be called from the Login Screen
     var onLoginSuccess: (() -> Unit)? = null
     var onSignupSuccess: (() -> Unit)? = null
 
@@ -36,6 +40,10 @@ class LoginViewModel(
         checkCurrentUser()
     }
 
+    /**
+     * Logs out the current user from Firebase and clears the user data.
+     * This function also clears the new notifications state.
+     */
     fun logout() {
         viewModelScope.launch {
             auth.signOut() // Sign out from Firebase
@@ -44,6 +52,11 @@ class LoginViewModel(
         }
     }
 
+    /**
+     * Checks if there is a currently authenticated user.
+     * If a user is found, it updates the state to reflect authentication and fetches user details.
+     * If no user is found, it sets the loading state to false.
+     */
     private fun checkCurrentUser() {
         val currentUser = auth.currentUser
         if (currentUser != null) {
@@ -54,6 +67,11 @@ class LoginViewModel(
         }
     }
 
+    /**
+     * Handles different login actions such as sign-in and sign-up.
+     *
+     * @param action The login action to perform, which can be either `SignIn` or `SignUp`.
+     */
     fun onAction(action: LoginAction){
         when(action) {
             is LoginAction.SignIn -> signIn(action.email, action.password)
@@ -61,6 +79,12 @@ class LoginViewModel(
         }
     }
 
+    /**
+     * Signs in a user with the provided email and password.
+     *
+     * @param email The email of the user.
+     * @param password The password of the user.
+     */
     private fun signIn(email: String, password:String) {
         _state.value = state.copy(isLoading = true)
 
@@ -91,6 +115,14 @@ class LoginViewModel(
         }
     }
 
+    /**
+     * Signs up a new user with the provided email, password, username, and name.
+     *
+     * @param email The email of the new user.
+     * @param password The password of the new user.
+     * @param username The username of the new user.
+     * @param name The name of the new user.
+     */
     private fun signUp(email: String, password: String, username:String, name:String) {
         val now = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
 
@@ -129,6 +161,11 @@ class LoginViewModel(
         }
     }
 
+    /**
+     * Creates a new user in the repository.
+     *
+     * @param user The user data to be created, represented as a `UserDto`.
+     */
     private fun createUser(user: UserDto) {
         viewModelScope.launch {
             println("CREATE USER : Create user")
@@ -152,6 +189,11 @@ class LoginViewModel(
         }
     }
 
+    /**
+     * Fetches user details from the repository using the provided email.
+     *
+     * @param email The email of the user to fetch.
+     */
     private fun getUser(email: String) {
         viewModelScope.launch {
             println("GET USER : Get user")
@@ -192,6 +234,11 @@ class LoginViewModel(
         }
     }
 
+    /**
+     * Updates the user details in the repository.
+     *
+     * @param user The user data to be updated, represented as a `UserDto`.
+     */
     private fun updateUser(user: UserDto) {
         viewModelScope.launch {
             println("UPDATE USER : Updated user ${user.id}")
